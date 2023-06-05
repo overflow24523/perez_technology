05ee2ff45a118d614a3251b6d3ad28c87547c9e0
Deivis <deivis@debian.deivistm>
2023-06-05 16:29:29 -0400

    working on ContactPhone

--- front/perez_technology/src/components/ContactPhone/ContactPhone.tsx
+++ front/perez_technology/src/components/ContactPhone/ContactPhone.tsx
@@ -1,7 +1,9 @@
 import './ContactPhone.css'
 const ContactPhone = () => {
     return <div className='ContactPhone'>
-               
+        <div className='screen'>
+            
+        </div>               
     </div>
 }
 
