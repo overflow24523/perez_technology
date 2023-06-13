f609ad17c58ba6ef96b58402de705d1f059aff77
Deivis <deivis@debian.deivistm>
2023-06-13 11:02:46 -0400

    chore(DTM/front): Inicio del componente Carousel Steam

---
+++ front/perez_technology/src/components/CarouselSteam/CarouselSteam.tsx
@@ -0,0 +1,9 @@
+import './CarouselSteam.css'
+const CarouselSteam = ()=> {
+    return <>
+        Aqui va el carousel steam
+    </>
+}
+
+
+export default  CarouselSteam 
---
+++ front/perez_technology/src/components/Talleres/Talleres.tsx
@@ -0,0 +1,11 @@
+import  './Talleres.css'
+import CarouselSteam from '../CarouselSteam/CarouselSteam';
+
+const Talleres = ()=>{
+    return <div className='Talleres'>
+        <CarouselSteam />
+    </div>
+}
+
+
+export default Talleres ; 
